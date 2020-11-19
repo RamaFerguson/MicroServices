01563ff9cc6ce2023373025c74335c60b80ca18d
root <root@LAPTOP-FNMN6R8O.localdomain>
2020-11-19 11:14:13 -0800

    external loggine

--- receiver/app.py
+++ receiver/app.py
@@ -13,14 +13,29 @@ from os import path
 MAX_EVENTS = 10
 EVENTS_FILENAME = "events.json"
 
+import os
+
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-with open('log_conf.yml', 'r') as f:
+# External Logging Configuration
+with open('log_conf_file', 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 
 def order_existing_customer(body):
